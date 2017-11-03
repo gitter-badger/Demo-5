cf2cb947225654f700275be9d514bc9ccd25b763
antonov-andrey144 <antonov-andrey144@yandex.ru>
2017-11-03 17:01:07 +0300

    comment

--- binaryTreeVS/binaryTree.h
+++ binaryTreeVS/binaryTree.h
@@ -1,1 +1,17 @@
 #pragma once
+
+struct BSTNode;
+
+struct BinarySearchTree
+{
+	BSTNode *root;
+};
+
+BinarySearchTree *createTree();
+void clearTree(BinarySearchTree *tree);
+
+bool contains(BinarySearchTree *tree, int value);
+bool add(BinarySearchTree *tree, int value);
+bool remove(BinarySearchTree *tree, int value);
+
+void print(BinarySearchTree *tree);
