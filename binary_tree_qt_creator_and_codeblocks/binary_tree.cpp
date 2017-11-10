2f42f473b5fc95c5aaea837508afdec42224250e
reacheight <reacheight8@gmail.com>
2017-11-10 16:23:48 +0300

    fixed printTree

--- binary_tree_qt_creator_and_codeblocks/binary_tree.cpp
+++ binary_tree_qt_creator_and_codeblocks/binary_tree.cpp
@@ -12,14 +12,15 @@ struct Tree {
 
 void printTree(Tree *tree, bool isUp)
 {
-
+    printRecursive(tree->root, isUp);
 }
 
 void printRecursive(Node* root, bool isUp)
 {
-    if (isUp)
+    if (root)
     {
-        printRecursive(root->leftChild);
-        if ()
+        printRecursive((isUp) ? root->leftChild : root->rightChild, isUp);
+        cout << root->value << " ";
+        printRecursive((isUp) ? root->rightChild : root->leftChild, isUp);
     }
 }
