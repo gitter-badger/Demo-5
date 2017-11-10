18d55ab607cdc3f7f071b469fda4d2dd2855e53b
reacheight <reacheight8@gmail.com>
2017-11-10 16:21:06 +0300

    added printTree function

--- binary_tree_qt_creator_and_codeblocks/binary_tree.cpp
+++ binary_tree_qt_creator_and_codeblocks/binary_tree.cpp
@@ -9,3 +9,17 @@ struct Node {
 struct Tree {
     Node* root;
 };
+
+void printTree(Tree *tree, bool isUp)
+{
+
+}
+
+void printRecursive(Node* root, bool isUp)
+{
+    if (isUp)
+    {
+        printRecursive(root->leftChild);
+        if ()
+    }
+}
