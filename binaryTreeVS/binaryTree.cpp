dc1bea68dbff3d625c829ace12733e22222ac3dd
antonov-andrey144 <antonov-andrey144@yandex.ru>
2017-11-10 16:35:57 +0300

    added contains

--- binaryTreeVS/binaryTree.cpp
+++ binaryTreeVS/binaryTree.cpp
@@ -10,3 +10,30 @@ struct Node {
 struct Tree {
 	Node *root;
 };
+
+bool containsNode(Node *node, int value)
+{
+	if (node == nullptr)
+	{
+		return false;
+	}
+
+	if (node->value == value)
+	{
+		return true;
+	}
+
+	if (value < node->value)
+	{
+		return containsNode(node->left, value);
+	}
+	else
+	{
+		return containsNode(node->right, value);
+	}
+}
+
+bool contains(Tree *tree, int value)
+{
+	return containsNode(tree->root, value);
+}
