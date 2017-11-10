6b8a44f00308b921c255f485550c71fc823137c9
Ilya Balashov <ibalashov24@gmail.com>
2017-11-10 16:21:10 +0300

    Push (shit edition)

--- binary_tree_qt_creator_and_codeblocks/binary_tree.cpp
+++ binary_tree_qt_creator_and_codeblocks/binary_tree.cpp
@@ -9,3 +9,39 @@ struct Node {
 struct Tree {
     Node* root;
 };
+
+void push(Tree *tree, int value)
+{
+    auto currentPosition = tree.root;
+    auto nextPosition = tree.root;
+    while (nextPosition != nullptr)
+    {
+        currentPosition = nextPosition;
+
+        if (value >= currentPosition->value)
+        {
+            nextPosition = currentPosition->rightChild;
+        }
+        else
+        {
+            nextPosition = currentPosition->leftChild;
+        }
+    }
+
+    Node *newNode = new Node;
+    newNode->leftChild = newNode->rightChild = nullptr;
+    newNode->value = value;
+
+    if (currentPosition == nullptr)
+    {
+        tree->root = newNode;
+    }
+    else if (nextPosition == currentPosition->rightChild)
+    {
+        currentPosition->rightChild = newNode;
+    }
+    else
+    {
+        currentPosition->leftChild = newNode;
+    }
+}
