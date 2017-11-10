1d93507e873e561e312d0809d416fdfdb0605597
katerina-kamkova <katerina.kamkova@mail.ru>
2017-11-10 16:36:12 +0300

    Done deleteElement

--- binary_tree_qt_creator_and_codeblocks/binary_tree.cpp
+++ binary_tree_qt_creator_and_codeblocks/binary_tree.cpp
@@ -9,3 +9,39 @@ struct Node {
 struct Tree {
     Node* root;
 };
+
+Node* findElement(Tree* tree, int value) {
+    Node* temp = tree->root;
+    while(temp->value != value) {
+        if (value < temp->value) {
+            temp = temp->leftChild;
+        } else {
+            temp = temp->rightChild;
+        }
+        if (temp == NULL) {
+            break;
+        }
+    }
+    return temp;
+}
+
+void deleteElement(Tree *tree, int value) {
+    Node* temp = findElement(tree, value);
+    if (temp == NULL) {
+        cout << "Error! No such element";
+    } else {
+        Node* changingElement = temp;
+        if (leftChild != NULL) {
+            while(rightChild != NULL) {
+                changingElement = changingElement->rightChild;;
+            }
+            swap(temp->value, changingElement->value);
+        } else if (rightChild != NULL) {
+            while(leftChild != NULL) {
+                changingElement = changingElement->leftChild;;
+            }
+            swap(temp->value, changingElement->value);
+        }
+        delete changingElement;
+    }
+}
